ca2c67efe9ae3a26f12905104880e1f436a26acb
Laurence Trippen <laurence.trippen@3dmaximal.com>
2019-03-06 16:49:50 +0100

    basic layout pool manager

--- utilities/poolsaddin/poolsaddin.py
+++ utilities/poolsaddin/poolsaddin.py
@@ -19,15 +19,51 @@ class MainWindow(QtWidgets.QWidget):
         # Window icon
         iconpath = cgruutils.getIconFileName('afanasy')
         if iconpath is not None:
-            self.setWindowIcon( QtGui.QIcon(iconpath))
+            self.setWindowIcon(QtGui.QIcon(iconpath))
+
+        poolsLabel = QtWidgets.QLabel("Pools")
+
+        poolsList = QtWidgets.QListWidget()
+        poolsList.addItem("Pool 1")
+
+        createPoolButton = QtWidgets.QPushButton("Create")
+        deletePoolButton = QtWidgets.QPushButton("Delete")
+        editPoolButton = QtWidgets.QPushButton("Edit")
+
+        poolsButtonLayout = QtWidgets.QHBoxLayout()
+        poolsButtonLayout.addWidget(createPoolButton)
+        poolsButtonLayout.addWidget(editPoolButton)
+        poolsButtonLayout.addWidget(deletePoolButton)
+
+        poolsLayout = QtWidgets.QVBoxLayout()
+        poolsLayout.addWidget(poolsLabel)
+        poolsLayout.addWidget(poolsList)
+        poolsLayout.addLayout(poolsButtonLayout)
+
+        clientsLabel = QtWidgets.QLabel("Clients")
+        
+        clientsList = QtWidgets.QListWidget()
+        clientsList.addItem("Client 01")
+
+        addClientButton = QtWidgets.QPushButton("Add")
+        removeClientButton = QtWidgets.QPushButton("Remove")
+
+        clientsButtonLayout = QtWidgets.QHBoxLayout()
+        clientsButtonLayout.addWidget(addClientButton)
+        clientsButtonLayout.addWidget(removeClientButton)
+
+        clientsLayout = QtWidgets.QVBoxLayout()
+        clientsLayout.addWidget(clientsLabel)
+        clientsLayout.addWidget(clientsList)
+        clientsLayout.addLayout(clientsButtonLayout)
+
+        listsLayout = QtWidgets.QHBoxLayout()
+        listsLayout.addLayout(poolsLayout)
+        listsLayout.addLayout(clientsLayout)
 
         # Top Root Layout
         topLayout = QtWidgets.QVBoxLayout(self)
-
-        poolsList = QtWidgets.QListWidget()
-        poolsList.addItem("Test")
-
-        topLayout.addWidget(poolsList)
+        topLayout.addLayout(listsLayout)
 
 app = QtWidgets.QApplication(sys.argv)
 
