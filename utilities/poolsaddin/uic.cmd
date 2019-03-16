a9a98bfaeaf58ae7e43e7735bc0ac7523a2181a1
Laurence Trippen <laurence.trippen@gmail.com>
2019-03-16 14:25:49 +0100

    Delete uic.cmd

--- utilities/poolsaddin/uic.cmd
+++
@@ -1,8 +0,0 @@
-@echo off
-
-echo %CGRU_LOCATION%
-
-set PYSIDE_UIC_EXE=%CGRU_LOCATION%\python\Scripts\pyside-uic.exe
-set POOLSADDIN_UI=%CGRU_LOCATION%\utilities\poolsaddin\ui
-
-call %PYSIDE_UIC_EXE% -o %POOLSADDIN_UI%\mainwindow.py %POOLSADDIN_UI%\mainwindow.ui
