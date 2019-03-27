52290b27fa83b3d55cb5c9e675fd8123b1f2c9c2
Laurence Trippen <laurence.trippen@3dmaximal.com>
2019-03-27 20:50:22 +0100

    implemented stateless untested database query TCP protocol

--- utilities/poolssupport/poolserver/server.py
+++ utilities/poolssupport/poolserver/server.py
@@ -4,16 +4,35 @@
 # E-Mail: laurence.trippen@gmail.com
 # Program: Afanasy Pool Server - Server
 
-import socket
+import os
 import sys
-import threading
 import json
-import pymongo
+import socket
+import threading
+import af
+
+from bson.json_util import dumps
+
+def get_mongodb_config():
+    path = os.path.join(os.environ["CGRU_LOCATION"], "utilities", "poolssupport", "poolmanager", "config.json")
+    config = {
+        "host":"localhost",
+        "port":"27017"
+    }
+    with open(path) as json_file:
+        data = json.load(json_file)
+        config["host"] = data["mongodb_host"]
+        config["port"] = data["mongodb_port"]
+    return config
 
 def server_log(msg):
     print("[AF Pool Server]:\t" + msg)
 
 class PoolServer():
+    def __init__(self):
+        self.mongodb_host = "localhost"
+        self.mongodb_port = "27017"
+
     def bind(self, host, port):
         try:
             self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -43,11 +62,26 @@ class PoolServer():
                 if data:
                     msg = data.decode("utf-8")
                     if msg == "get_all_pools()":
-                        pass
+                        af.MongoDB.connection = af.MongoDB()
+                        af.MongoDB.connection.connect("mongodb://" + self.mongodb_host + ":" + self.mongodb_port)
+                        pools = af.MongoDB.connection.findAllPools()
+                        pools_json = dumps(pools)
+                        client.send(pools_json.encode())
+                client.close()
             except:
                 client.close()
 
 if __name__ == "__main__":
+    if "CGRU_LOCATION" in os.environ:
+        server_log("CGRU_LOCATION=" + os.environ['CGRU_LOCATION'])
+    else:
+        server_log("CGRU_LOCATION is not set!")
+        sys.exit()
+
+    mongodb_config = get_mongodb_config()
+
     poolServer = PoolServer()
+    poolServer.mongodb_host = mongodb_config["host"]
+    poolServer.mongodb_port = mongodb_config["port"]
     poolServer.bind("", 9999)
     poolServer.listen(10)
