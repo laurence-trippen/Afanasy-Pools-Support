01ef2eb2f791afb9874e7407df46809b5540de6b
Laurence Trippen <laurence.trippen@3dmaximal.com>
2019-03-29 13:36:31 +0100

    added comments to protocol.py

--- utilities/poolssupport/poolserver/protocol.py
+++ utilities/poolssupport/poolserver/protocol.py
@@ -1,8 +1,17 @@
+# Encoding: UTF-8
+# Author: Laurence Trippen
+# Date: 25.03.2019
+# E-Mail: laurence.trippen@gmail.com
+# Program: Afanasy Pool Server - Network protocol
+# Defines the rules of a simple stateless request/response network
+# protocol based on TCP.
+
 import json
 
+# Get pools command
 GET_POOLS = "GET POOLS"
 
-
+# Generates a json request
 def request(command):
     req = {
         "type": "request",
@@ -10,7 +19,7 @@ def request(command):
     }
     return json.dumps(req)
 
-
+# Generates a json response
 def response(size):
     res = {
         "type": "response",
