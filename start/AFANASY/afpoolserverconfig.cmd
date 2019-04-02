ffda12c6da18b83b9d738e7b631f3561905e27f3
Laurence Trippen <laurence.trippen@3dmaximal.com>
2019-04-02 13:50:37 +0200

    CGRU Keeper pool section

--- start/AFANASY/afpoolserverconfig.cmd
+++ start/AFANASY/afpoolserverconfig.cmd
@@ -1,6 +1,6 @@
 rem Name=Configure Pool Server
 rem Icon=afanasy.png
-rem Separator
+
 call %0\..\_setup.cmd
 
 "%CGRU_PYTHONEXE%" "%CGRU_LOCATION%\utilities\poolssupport\poolserver\configurator.py" %*
